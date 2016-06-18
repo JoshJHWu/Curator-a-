33e8d9795c853cd2908de80109f922d10e6cae11
Esther Leytush <eleytush@gmail.com>
2016-06-18 15:54:28 -0400

    add key

--- curator/app/controllers/dashboard_controller.rb
+++ curator/app/controllers/dashboard_controller.rb
@@ -9,9 +9,11 @@ class DashboardController < ApplicationController
 
     # makes a call to the Reddit API
     reddit_data = call_to_Reddit(term)
+    reddit_data = call_to_HPE(reddit_data)
 
     # makes a call to the NYTimes API
     news_data = call_to_News(term)
+    news_data = call_to_HPE(news_data)
 
     # return what we got in a json
   end

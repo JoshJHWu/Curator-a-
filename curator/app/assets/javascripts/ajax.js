da5ff94cd9440d07bf9dcc10828ff58a734ab6de
Lawson Marlowe <lawsonmarlowe@gmail.com>
2016-06-19 11:46:45 -0400

    Ajax working for news and reddit links

---
+++ curator/app/assets/javascripts/ajax.js
@@ -0,0 +1,39 @@
+var partialRequest = function(newsInfo, redditInfo) {
+	$.ajax({
+		type: "get",
+	  url: "/html",
+	  data: $(this).serialize()
+	}).done(function(partialResults) {
+		$(".yielded-index").html(partialResults);
+		for(i = 0; i < redditInfo.length; i++) {
+			$("#reddit-card-action ul").append("<li><a href=" + redditInfo[i].url + "></a></li>");
+			$("#reddit-card-action ul li").eq(i).children().text(redditInfo[i].title);
+		};
+		for(i = 0; i < newsInfo.length; i++) {
+			$("#news-card-action ul").append("<li><a href=" + newsInfo[i].url + "></a></li>");
+			$("#news-card-action ul li").eq(i).children().text(newsInfo[i].title); 
+		};
+	});
+};
+
+$(document).ready(function() {
+
+	var newsInfo;
+	var redditUrls;
+
+	$("body").on("submit", "form", function() {
+  	$(".yielded-index").html("Loading...");
+			event.preventDefault();
+			$.ajax({
+	      type: "get",
+	      url: "/search",
+	      data: $(this).serialize(),
+	      datatype: 'json'
+	    }).done(function(results) {
+    		newsInfo = results.news.posts;
+    		redditInfo = results.reddit.posts;
+	    	console.log(results);
+	    	partialRequest(newsInfo, redditInfo);
+	    });
+	});
+});

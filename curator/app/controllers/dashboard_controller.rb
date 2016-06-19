e368d55c5e172ee7685fecc96cd05af2db678cd4
Esther Leytush <eleytush@gmail.com>
2016-06-19 10:12:40 -0400

    tested as API endpoint

--- curator/app/controllers/dashboard_controller.rb
+++ curator/app/controllers/dashboard_controller.rb
@@ -5,20 +5,20 @@ class DashboardController < ApplicationController
   end
 
   def search
-    @term = params[:term].gsub(" ", "+")
+    term = params[:term].gsub(" ", "+")
 
-    reddit_data = call_to_Reddit(@term)
-    news_data = call_to_News(@term)
-
-    render json: {news: news_data, reddit: reddit_data}.to_json, layout: false
-  end
-
-  def json
-    @term = params[:term].gsub(" ", "+")
-
-    reddit_data = call_to_Reddit(@term)
-    news_data = call_to_News(@term)
+    reddit_data = call_to_Reddit(term)
+    news_data = call_to_News(term)
 
     render json: {news: news_data, reddit: reddit_data}.to_json, layout: false
   end
+
+  # def json
+  #   @term = params[:term].gsub(" ", "+")
+  #
+  #   reddit_data = call_to_Reddit(@term)
+  #   news_data = call_to_News(@term)
+  #
+  #   render json: {news: news_data, reddit: reddit_data}.to_json, layout: false
+  # end
 end

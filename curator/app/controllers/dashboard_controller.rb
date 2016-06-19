78f9ff4cab6319c2719faea959fef19745ee99c2
Esther Leytush <eleytush@gmail.com>
2016-06-18 23:15:42 -0400

    remove hpo

--- curator/app/controllers/dashboard_controller.rb
+++ curator/app/controllers/dashboard_controller.rb
@@ -4,18 +4,13 @@ class DashboardController < ApplicationController
   end
 
   def search
-    # takes the param - search term
     term = params[:term]
 
-    # makes a call to the Reddit API
     reddit_data = call_to_Reddit(term)
-    reddit_data = call_to_HPE(reddit_data)
-
-    # makes a call to the NYTimes API
     news_data = call_to_News(term)
-    news_data = call_to_HPE(news_data)
+
+    json = {news: news_data, reddit: reddit_data}
 
     # return what we got in a json to the frontend
-    
   end
 end

8bba49985067105daf719f1d13b1eb4572f50f44
Esther Leytush <eleytush@gmail.com>
2016-06-19 11:37:39 -0400

    add jquery word cloud documentation

--- curator/app/assets/javascripts/word_cloud.js
+++ curator/app/assets/javascripts/word_cloud.js
@@ -2,4 +2,7 @@
 // the word array will be in the JSON as "news_array" and "reddit_array"
 // #example should be where it is appended
 
+
+// documentation for the word cloud: https://github.com/lucaong/jQCloud
+
 $("#example").jQCloud(word_array);
